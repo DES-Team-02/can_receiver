a7ae252313a179af239916ef69ffc133ee9a84da
kianwasabi <55065075+kianwasabi@users.noreply.github.com>
2024-01-24 19:22:09 +0100

    Change CAN interface from can0 to can1

--- can-setup.sh
+++ can-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set CAN interface 
-sudo ip link set can0 up type can bitrate 500000
+sudo ip link set can1 up type can bitrate 500000
 
 # Set txqueuelen 
-sudo ifconfig can0 txqueuelen 65536
+sudo ifconfig can1 txqueuelen 65536
